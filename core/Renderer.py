e980bfac7354b492bc86d72b11d0dd1470635d9f
Muhammad Farooq <mfarooq102005@gmail.com>
2025-05-03 23:46:04 +0500

    feat: Add Renderer class

---
+++ core/Renderer.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame import Surface
+from core.LayerSystem import LayerStack
+
+BLACK = (0, 0, 0)
+
+
+class Renderer:
+    def __init__(self, screen: Surface):
+        self.screen = screen
+
+    def clear(self):
+        self.screen.fill(BLACK)
+
+    def flip(self):
+        pygame.display.flip()
+
+    def create_surface(self, width, height):
+        return pygame.Surface((width, height), pygame.SRCALPHA, 32)
+
+    def submit_surface(self, surface: Surface, x=0, y=0):
+        self.screen.blit(surface, (x, y))
+
+    def show(self):
+        pygame.display.flip()
